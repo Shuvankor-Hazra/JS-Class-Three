2b32d93de19136d900bea06330a983e8a6596ead
Shuvankor-Hazra <shuvo.s.ovuhs@gmail.com>
2022-12-17 10:41:50 +0600

    JavaScript Class Three Compleated

---
+++ script.js
@@ -0,0 +1,149 @@
+// // 1. Comparison Operators
+// // ==
+// console.log(9 == 9);
+// console.log(9 == 8);
+// console.log(9 == "9");
+
+// // ===
+// console.log(9 === "9");
+// console.log("9" === "9");
+
+// // !=
+// console.log(9 != 9);
+// console.log(9 != 8);
+
+// // !==  (Not equal value or not equal type)
+// console.log(9 !== 9);
+// console.log(9 !== 8);
+// console.log(9 !== "9");
+
+// // >
+// console.log(9 > 8);
+// console.log(9 > 9);
+// console.log(9 > 10);
+
+// // <
+// console.log(9 < 8);
+// console.log(9 < 9);
+// console.log(9 < 10);
+
+// // >=
+// console.log(9 >= 8);
+// console.log(9 >= 9);
+// console.log(9 >= 10);
+
+// // <=
+// console.log(9 <= 8);
+// console.log(9 <= 9);
+// console.log(9 <= 10);
+
+// // ? Ternary Operators
+
+// 2. Logical Operators
+// &&
+// ||
+// !
+
+// // Data && Data
+// let x = 6;
+// let y = 2;
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 && y > 3);
+
+// // Data || Data
+// console.log(x < 9);
+// console.log(y > 3);
+// console.log(x < 9 || y > 3);
+
+// // Data ! Data
+// console.log(x == y);
+// console.log(!(x == y));
+
+// 3. Opreators Precedence
+// B O D M A S
+// console.log((36 / 6) * 3 + 2 ** 2 - (3 + 5));
+// 6 * 3 + 2 ** 2 - 8
+// 6 * 3 + 4 - 8
+// 18 + 4 - 8
+// 22 - 8
+// 14
+
+// // 4. Template Literals
+// const firstName = "Shuvankor";
+// const lastName = "Hazra";
+// let age = 30;
+// let job = "Web Developer";
+// const country = "Bangladesh";
+
+// // I'm Shuvankor Hazra, 30 years old Web Developer!
+// // console.log("I'm Shuvankor Hazra, 30 years old Web Developer!");
+// // console.log(
+// //   "I'm " + firstName + " " + lastName + ", " + age + " years old " + job + "!"
+// // );
+// console.log("I'm Shuvankor Hazra, 30 years old Web Developer!");
+// console.log(`I'm ${firstName} ${lastName}, ${age} years old ${job}!`);
+// console.log(`I love ${country}!`);
+
+// console.log(`First line
+// Second line
+// Third line
+// Fourth line
+// Fifth line`);
+
+// console.log(`Bangladesh win ${2 + 1} series`);
+
+// // 5. If / Else Statements
+// if(){
+
+// }else{
+
+// }
+
+// if (5 > 3) {
+//   console.log("5");
+// } else {
+//   console.log("Something Wrong");
+// }
+
+// if (5 < 3) {
+//   console.log("5");
+// } else {
+//   console.log("Something Wrong");
+// }
+
+// if (10 < 15) {
+//   console.log("Display 10");
+// } else {
+//   console.log("Something Wrong");
+// }
+
+// if (10 > 15) {
+//   console.log("Display 10");
+// } else {
+//   console.log("Something Wrong");
+// }
+
+// let adritaAge = 6;
+
+// if (adritaAge >= 6) {
+//   console.log(`Adrita can start school!`);
+// } else {
+//   console.log(`Adrita can't start school!`);
+// }
+
+// let adritaAge = 5;
+
+// if (adritaAge >= 6) {
+//   console.log(`Adrita can start school!`);
+// } else {
+//   console.log(`Adrita can't start school!`);
+// }
+
+// let adritaAge = 7;
+
+// if (adritaAge >= 6) {
+//   console.log(`Adrita can start school!`);
+// } else {
+//   console.log(`Adrita can't start school!`);
+// }
